d4e71e52b60e79afd91e6b5db311c585fd29c4f9
Kayla Inoa <kaylainoa@Kaylas-MacBook-Air.local>
2025-10-14 15:21:56 -0400

    feat(portfolio): added portfolio page and PortfolioCard component

---
+++ frontend/src/app/components/PortfolioCard.tsx
@@ -0,0 +1,21 @@
+export default function PortfolioCard() {
+    return (
+    <div>
+        <img src="/path/to/image.jpg" alt="Project Image" />
+        <h2>Project Title</h2>
+        <p>Short description of the project.</p>
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+    );
+}
---
+++ frontend/src/app/portfolio/page.tsx
@@ -0,0 +1,25 @@
+import PortfolioCard from "../components/PortfolioCard";
+
+export default function PortfolioPage() {
+    return (
+        <div>
+
+            <div>Portfolio Page</div>
+
+            <div> <PortfolioCard /> </div>
+
+
+        </div>
+        
+        
+
+
+
+
+
+
+
+
+
+    );
+}
