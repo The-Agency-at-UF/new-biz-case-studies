ceeb683af61a2b64cdc44b73f35f0f9570c296f7
Kayla Inoa <kaylainoa@Kaylas-MacBook-Air.local>
2025-10-14 18:26:29 -0400

    refactor(portfolio): more cards +  comments for sections

--- frontend/src/app/components/PortfolioCard.tsx
+++ frontend/src/app/components/PortfolioCard.tsx
@@ -1,7 +1,7 @@
 export default function PortfolioCard() {
     return (
-    <div className="gradient-outline inline-block p-6 rounded-lg">
-        <img src="/path/to/image.jpg" alt="Project Image" className="size-56"/>
+    <div className="gradient-outline inline-block p-6 rounded-xl">
+        <img src="/path/to/image.jpg" alt="Project Image" className="size-56 rounded-lg"/>
         <h2 className="font-bold">Project Title</h2>
         <p>Short description of the project.</p>
 
--- frontend/src/app/portfolio/page.tsx
+++ frontend/src/app/portfolio/page.tsx
@@ -3,11 +3,23 @@ import PortfolioCard from "../components/PortfolioCard";
 export default function PortfolioPage() {
     return (
         <div>
+            <div> 
+                {/* Where nav bar will go */}
+            </div>
 
-            <div>Portfolio Page</div>
-
-            <div> <PortfolioCard /> </div>
-
+            <div>
+                {/* Hero section */}
+                <div>Portfolio Page</div>
+            </div>
+            
+            <div> {/* Buttons for category */} </div>
+            <div className="flex flex-wrap gap-10"> 
+                <PortfolioCard /> 
+                <PortfolioCard /> 
+                <PortfolioCard /> 
+                <PortfolioCard /> 
+                <PortfolioCard /> 
+            </div>
 
         </div>
         
