b3414e2c642f30b6b39717550cb3d03e4961fee8
Jess <jessanderson.1145@gmail.com>
2025-10-28 11:12:40 -0400

    fix: merge conflicts, migrated portfolio page into /portfolio

--- frontend/src/app/portfolio/page.tsx
+++ frontend/src/app/portfolio/page.tsx
@@ -1,18 +1,17 @@
 import PortfolioCard from "../components/PortfolioCard";
+import PortfolioBar from "../components/PortfolioBar";
 
 export default function PortfolioPage() {
     return (
         <div>
-            <div> 
-                {/* Where nav bar will go */}
-            </div>
+            <PortfolioBar />
 
             <div>
                 {/* Hero section */}
                 <div>Portfolio Page</div>
             </div>
             
-            <div className="flex flex-wrap gap-3"> {/* Buttons for category */} 
+            <div className="mt-16 flex flex-wrap gap-3"> {/* Buttons for category */} 
 
                 <button className="px-4 py-2 bg-white text-black font-bold rounded-md">All</button>
                 <button className="px-4 py-2 bg-white text-black font-bold rounded-md">Category</button>
@@ -20,7 +19,7 @@ export default function PortfolioPage() {
                 <button className="px-4 py-2 bg-white text-black font-bold rounded-md">Category</button>
                 <button className="px-4 py-2 bg-white text-black font-bold rounded-md">Category</button>
             </div>
-            <div className="flex flex-wrap gap-10"> 
+            <div className="mt-16 flex flex-wrap gap-10"> 
                 <PortfolioCard /> 
                 <PortfolioCard /> 
                 <PortfolioCard /> 
