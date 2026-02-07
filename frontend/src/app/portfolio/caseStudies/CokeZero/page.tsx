66b0a00ac0339c3810007dd5fb446c6422c1cd15
Kayla Inoa <kinoa718@gmail.com>
2026-02-07 16:43:20 -0500

    feat(cokeTheOpportunity) Opportunity section created

--- frontend/src/app/portfolio/caseStudies/CokeZero/page.tsx
+++ frontend/src/app/portfolio/caseStudies/CokeZero/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import cokeBackdrop from "./assets/CokeBackdrop.jpg";
+import cokeZero from "./assets/CokeZeroWhite.png";
+import cokeAsset5 from "./assets/CokeAsset5.png";
 import agencyLogo from "./assets/AgencyLogoFull.png";
 import NavBar from "../../../../components/NavBar";
 
@@ -7,50 +9,92 @@ export default function CokeZeroPage() {
   return (
     <>
       <NavBar />
+      <div className="mx-20">
 
-      <section
-        id="coke-landing"
-        className="relative min-h-screen flex items-center overflow-hidden"
-      >
-        {/* Background Image */}
-        <div
-          className="absolute inset-0 bg-cover bg-center"
-          style={{ backgroundImage: `url(${cokeBackdrop.src})` }}
-        />
+        <section
+          id="coke-landing"
+          className="relative min-h-screen flex items-center overflow-hidden"
+        >
+          {/* Background Image */}
+          <div
+            className="absolute inset-0 bg-cover bg-center"
+            style={{ backgroundImage: `url(${cokeBackdrop.src})` }}
+          />
 
-        {/* Gradient Overlay */}
-        <div className="absolute inset-0 bg-gradient-to-b 
-                        from-black/40 
-                        via-black/70 
-                        to-[#0E0E0E]" />
+          {/* Gradient Overlay */}
+          <div className="absolute inset-0 bg-gradient-to-b 
+                          from-black/40 
+                          via-black/70 
+                          to-[#0E0E0E]" />
 
-        {/* Content */}
-        <div className="relative z-10 text-white w-full max-w-6xl px-8">
-          <p className="inline-block font-bold outline outline-[3px] outline-[#ED1C24] px-3 py-2">
-            Case Study
-          </p>
+          {/* Content */}
+          <div className="relative z-10 text-white w-full max-w-6xl px-8">
+            <p className="inline-block font-bold outline outline-[3px] outline-[#ED1C24] px-3 py-1">
+              Case Study
+            </p>
 
-          <div className="flex items-center gap-4 mt-6">
-            <p className="font-bold text-lg">X</p>
+            <div className="flex items-center gap-4 mt-6">
+              <Image
+                src={cokeZero}
+                alt="Coca Cola Zero Sugar"
+                className="w-60 h-auto pr-2"
+              />
 
-            <Image
-              src={agencyLogo}
-              alt="The Agency at the University of Florida"
-              className="w-80 h-auto"
-            />
+              <p className="font-bold text-lg">X</p>
+
+              <Image
+                src={agencyLogo}
+                alt="The Agency at the University of Florida"
+                className="w-80 h-auto pl-2"
+              />
+            </div>
+
+            <div className="w-200 h-[3px] bg-[#ED1C24] mt-6" />
+
+            <h3 className="text-3xl font-bold mt-6">
+              Developing Digital Solutions to Punt the Pandemic
+            </h3>
           </div>
+        </section>
+        <section className="relative min-h-screen items-center overflow-hidden ml-2 ">
+          <div className="py-6">
+            <h1 className="text-5xl font-black">THE</h1>
+            <h1 className="text-5xl text-[#DA2028] font-black">OPPORTUNITY</h1>
+          </div>
+          
+          <div className="flex">
+            <div className="w-150">
+              <p>In 2020, the possibility of a safe and normal college football season was 
+              intercepted by the COVID-19 pandemic. Fans watched their favorite players test
+              positive, their favorite teams pull out and anticipated games get cancelled
+              indefinitely. Many had to say goodbye to their game-day favorites, namely rowdy
+              crowds and over-the-top tailgates.
+              </p>
+              <br></br>
+              <p>
+                Like the rest of us, Coca-Cola did not anticipate a pandemic, much less one that would 
+                leave an impact for years to come. Coke's internal marketing agency, KO:OP, had planned
+                various in-person activations inside college football stadiums for the 2020 season and 
+                beyond. These needed to be adjusted. Given in-person restrictions and emerging health 
+                concerns, Coke’s team was on a mission to find the best ways to nuance its means of 
+                refreshing football fanatics in 2021.
+              </p>
+            </div>
+            <div className="relative w-130 h-130 rounded-full overflow-hidden">
+              <Image
+                src={cokeAsset5}
+                alt="Game day coke advertisement"
+                fill
+                className="object-cover object-[20%_50%]"
+              />
+            </div>
+            
+          </div>
+          
 
-          <div className="w-200 h-[3px] bg-[#ED1C24] mt-6" />
-
-          <h3 className="text-3xl font-bold mt-6">
-            Developing Digital Solutions to Punt the Pandemic
-          </h3>
-        </div>
-      </section>
-      <section>
-        <h1>The</h1>
-        <h1>Opportunity</h1>
-      </section>
+        
+        </section>
+      </div>
     </>
   );
 }
