430de4921107c07b69d00c008656c24d3b57c814
Kayla Inoa <kinoa718@gmail.com>
2026-02-07 16:47:34 -0500

    feat(font) added global gentona font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +12,23 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const gentona = localFont({
+  src: [
+    { path: "../fonts/Gentona Thin.otf", weight: "100" },
+    { path: "../fonts/Gentona ExtraLight.otf", weight: "200" },
+    { path: "../fonts/Gentona Light.otf", weight: "300" },
+    { path: "../fonts/Gentona Book.otf", weight: "400" },
+    { path: "../fonts/Gentona Medium.otf", weight: "500" },
+    { path: "../fonts/Gentona SemiBold.otf", weight: "600" },
+    { path: "../fonts/Gentona Bold.otf", weight: "700" },
+    { path: "../fonts/Gentona ExtraBold.otf", weight: "800" },
+    { path: "../fonts/Gentona Heavy.otf", weight: "900" },
+  ],
+});
+
+
+
 
 export const metadata: Metadata = {
   title: "The Agency at UF, Case Studies",
