6571a85659c444471ddd3cd3470069df6e1c4119
hiralshukla <hiralshukla365@gmail.com>
2026-02-11 23:48:56 -0500

    feat: page init for admin and login
    help Neema start auth development

---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../../components/NavBar";
+export default function AdminPage() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center text-white">
+          <NavBar />
+    
+          <div>
+            <h1 className="text-foreground">Admin Page under construction... Check back soon!</h1>
+          </div>
+        </div>
+  );
+}
