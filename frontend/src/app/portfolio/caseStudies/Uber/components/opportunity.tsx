ca537a86f1322936f9c365f21b0678a7c0c07fa2
Adora Lin <adoraloin@gmail.com>
2026-03-04 16:35:55 -0500

    feat: fixed spacing

--- frontend/src/app/portfolio/caseStudies/Uber/components/opportunity.tsx
+++ frontend/src/app/portfolio/caseStudies/Uber/components/opportunity.tsx
@@ -14,7 +14,7 @@ export default function OpportunitySection() {
           <span className="block text-[#76c893]">Opportunity</span>
         </h2>
              <p className={`${gentonaMedium.className} text-sm lg:text-4xl font-light text-white leading-tight tracking-wide`}>
-             We saw the chance to help the tech giant connect with more audiences.<br></br><br></br>
+             We saw the chance to help the tech giant connect with more audiences.<br></br>
             We <span className="font-bold">forecasted trends</span>, drew out a <span className="font-bold">monthly cultural pulse</span>, and <span className="font-bold">built bold assets.</span>
           </p>
         </div>
