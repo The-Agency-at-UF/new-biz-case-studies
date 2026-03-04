b3a778ae3c1696d0000e6368c99abe931fde0dbe
Endry Rodriguez <139926363+endryisnotcool@users.noreply.github.com>
2026-03-04 13:12:53 -0500

    test

--- frontend/src/app/portfolio/caseStudies/Bliss/page.tsx
+++ frontend/src/app/portfolio/caseStudies/Bliss/page.tsx
@@ -1,6 +1,7 @@
 //import Image from "next/image"; // use Next.js Image component for any images
 import NavBar from "../../../../components/NavBar";
 import BlissHero from "./components/hero";
+import OpportunitySection from "./components/opportunity";
 
 export default function BlissPage() {
   return (
