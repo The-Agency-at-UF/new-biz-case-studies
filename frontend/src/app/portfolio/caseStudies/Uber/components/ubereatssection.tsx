383b3759fa9917f0b0c73c24cff725e15fc5d18e
Adora Lin <adoraloin@gmail.com>
2026-03-10 22:59:02 -0400

    feat: implemented uber eats section

---
+++ frontend/src/app/portfolio/caseStudies/Uber/components/ubereatssection.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { gentonaMedium } from "@/app/fonts";
+
+export default function UberEatsSection() {
+  return (
+    <div className="relative flex flex-col h-full px-15 md:px-30 lg:px-40 inset-0 py-38">
+      <div className="max-w-none flex flex-row items-start items-center gap-6 lg:gap-18">
+        <h2
+          className={`${gentonaMedium.className} text-4xl lg:text-[8rem] font-black uppercase leading-[0.8] tracking-normal shrink-0`}
+        >
+          <span className="block text-white">Uber</span>
+          <span className="block text-[#76c893]">Eats</span>
+        </h2>
+        <p
+          className={`${gentonaMedium.className} text-sm lg:text-4xl font-light text-white leading-tight tracking-wide`}
+        >
+          Our team helped American and Canadian restaurants promote Uber Eats as one of their customers' dining options by creating adaptable promotional materials. These included digital templates for evergreen content as well as templates made to fit specific Uber Eats events and discounts.
+        </p>
+      </div>
+    </div>
+  );
+}
--- frontend/src/app/portfolio/caseStudies/Uber/page.tsx
+++ frontend/src/app/portfolio/caseStudies/Uber/page.tsx
@@ -4,6 +4,7 @@ import UberHero from "./components/hero";
 import OpportunitySection from "./components/opportunity";
 import UberBlackSection from "./components/uberblacksection";
 import PrivateHireBadge from "./components/privatehirebadge";
+import UberEatsSection from "./components/ubereatssection";
 
 export default function UberPage() {
   return (
@@ -14,6 +15,7 @@ export default function UberPage() {
       <OpportunitySection />
       <UberBlackSection />
       <PrivateHireBadge />
+      <UberEatsSection />
     </div>
   );
 }
