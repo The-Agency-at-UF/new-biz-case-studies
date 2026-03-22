7f8c17025775a6899d52197e9856c7a142556c28
Kayla Inoa <kinoa718@gmail.com>
2026-03-22 11:51:41 -0400

    feat: created Carnival Landing Section

---
+++ frontend/src/app/portfolio/caseStudies/Carnival/components/CarnivalLandingSection.tsx
@@ -0,0 +1,8 @@
+
+export default function CarnivalLandingSection() {
+  return (
+    <div className="min-h-screen text-foreground bg-background">
+        <h1>Coming Soon!</h1>
+    </div>
+  );
+}
